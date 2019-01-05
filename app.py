21fc504854b8a0118455d182f015d296290c0ed8
ptuls <paul.t@canva.com>
2019-01-05 22:00:32 +1100

    Finer control on burn in period

--- app.py
+++ app.py
@@ -27,17 +27,18 @@ def main():
     num_races = 10000
 
     probabilities = np.array([0.5, 0.1, 0.4])
-    odds = np.array([1.3, 10.0, 3.0])
+    odds = np.array([1.3, 7.0, 3.0])
 
     initial_wealth = 1
-    burn_in_period = round(0.3 * num_races)
+    burn_in_period = 100
     thompson_samp_portfolio = ThompsonSamplingPortfolio(len(odds), initial_wealth)
     optimal_portfolio = OptimalPortfolio(len(odds), initial_wealth)
 
     thompson_samp_portfolio_history = []
     optimal_portfolio_history = []
-    for race in range(num_races):
-        log.info('Trial: %d' % (race + 1))
+    total_trials = num_races + burn_in_period
+    for trial in range(total_trials):
+        log.info('Trial: %d' % (trial + 1))
         thompson_samp_portfolio.allocation = compute_optimal_allocation(
             thompson_samp_portfolio,
             odds,
@@ -53,14 +54,14 @@ def main():
         log.info('Horse %d wins' % win_index)
         thompson_samp_portfolio.update(win_index)
         optimal_portfolio.update(win_index)
-        if race > burn_in_period:
+        if trial > burn_in_period:
             thompson_samp_portfolio.update_wealth(win_index, odds)
             optimal_portfolio.update_wealth(win_index, odds)
 
             thompson_samp_portfolio_history.append(thompson_samp_portfolio.wealth)
             optimal_portfolio_history.append(optimal_portfolio.wealth)
 
-    log.info('-----------------------------')
+    log.info('------------------------------------------------')
     log.info('estimate, true, odds')
     estimates = thompson_samp_portfolio.mean()
     for i, estimate in enumerate(estimates):
@@ -68,19 +69,19 @@ def main():
 
     bandit_cagr = cagr(initial_wealth, thompson_samp_portfolio.wealth, num_races)
     optimal_cagr = cagr(initial_wealth, optimal_portfolio.wealth, num_races)
-    log.info('Final CAGR: {:.2f}%'.format(bandit_cagr))
+    log.info('Final CAGR (Thompson): {:.2f}%'.format(bandit_cagr))
     log.info('Final CAGR (optimal): {:.2f}%'.format(optimal_cagr))
 
-    plot_wealth(thompson_samp_portfolio_history, optimal_portfolio_history, num_races, burn_in_period)
+    plot_wealth(thompson_samp_portfolio_history, optimal_portfolio_history, num_races)
 
 
-def plot_wealth(thompson_samp_portfolio_history, optimal_portfolio_history, num_races, burn_in_period):
-    plot_range = np.arange(1, num_races - burn_in_period, 1)
+def plot_wealth(thompson_samp_portfolio_history, optimal_portfolio_history, num_races):
+    plot_range = np.arange(1, num_races, 1)
     fig, ax = plt.subplots()
-    ax.semilogy(plot_range, thompson_samp_portfolio_history, 'b', label='Bayesian')
+    ax.semilogy(plot_range, thompson_samp_portfolio_history, 'b', label='Thompson Samp')
     ax.semilogy(plot_range, optimal_portfolio_history, 'r', label='Optimal')
-    plt.xlabel('Trial')
-    plt.ylabel('Wealth')
+    plt.xlabel('Race')
+    plt.ylabel('Wealth ($)')
     plt.legend(loc='upper left')
     plt.show()
 
