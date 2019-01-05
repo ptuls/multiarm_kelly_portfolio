aec932b6edfad7015d14d4d80b53d9dfaa4a344f
ptuls <paul.t@canva.com>
2019-01-05 17:31:31 +1100

    Simplified function

--- app.py
+++ app.py
@@ -24,21 +24,25 @@ log.addHandler(ch)
 
 
 def main():
-    num_trials = 2000
+    num_races = 2000
 
     probabilities = np.array([0.5, 0.1, 0.4])
     odds = np.array([1.3, 10.0, 3.0])
 
     initial_wealth = 1
-    burn_in_period = round(0.3 * num_trials)
+    burn_in_period = round(0.3 * num_races)
     bayesian_portfolio = BayesianPortfolio(len(odds), initial_wealth)
     optimal_portfolio = OptimalPortfolio(len(odds), initial_wealth)
 
     bayesian_portfolio_history = []
     optimal_portfolio_history = []
-    for trial in range(num_trials):
-        log.info('Trial: %d' % (trial + 1))
-        bayesian_portfolio.allocation = compute_allocation(bayesian_portfolio, odds)
+    for race in range(num_races):
+        log.info('Trial: %d' % (race + 1))
+        bayesian_portfolio.allocation = compute_optimal_allocation(
+            bayesian_portfolio,
+            odds,
+            np.asarray(bayesian_portfolio.draw())
+        )
         optimal_portfolio.allocation = compute_optimal_allocation(
             optimal_portfolio,
             odds,
@@ -49,7 +53,7 @@ def main():
         log.info('Horse %d wins' % win_index)
         bayesian_portfolio.update(win_index)
         optimal_portfolio.update(win_index)
-        if trial > burn_in_period:
+        if race > burn_in_period:
             bayesian_portfolio.update_wealth(win_index, odds)
             optimal_portfolio.update_wealth(win_index, odds)
 
@@ -62,16 +66,16 @@ def main():
     for i, estimate in enumerate(estimates):
         log.info('%i: %f, %f, %f' % (i, estimate, probabilities[i], odds[i]))
 
-    bandit_cagr = cagr(initial_wealth, bayesian_portfolio.wealth, num_trials)
-    optimal_cagr = cagr(initial_wealth, optimal_portfolio.wealth, num_trials)
+    bandit_cagr = cagr(initial_wealth, bayesian_portfolio.wealth, num_races)
+    optimal_cagr = cagr(initial_wealth, optimal_portfolio.wealth, num_races)
     log.info('Final CAGR: {:.2f}%'.format(bandit_cagr))
     log.info('Final CAGR (optimal): {:.2f}%'.format(optimal_cagr))
 
-    plot_wealth(bayesian_portfolio_history, optimal_portfolio_history, num_trials, burn_in_period)
+    plot_wealth(bayesian_portfolio_history, optimal_portfolio_history, num_races, burn_in_period)
 
 
-def plot_wealth(bayesian_portfolio_history, optimal_portfolio_history, num_trials, burn_in_period):
-    plot_range = np.arange(1, num_trials - burn_in_period, 1)
+def plot_wealth(bayesian_portfolio_history, optimal_portfolio_history, num_races, burn_in_period):
+    plot_range = np.arange(1, num_races - burn_in_period, 1)
     fig, ax = plt.subplots()
     ax.semilogy(plot_range, bayesian_portfolio_history, 'b', label='Bayesian')
     ax.semilogy(plot_range, optimal_portfolio_history, 'r', label='Optimal')
@@ -87,20 +91,6 @@ def simulate(probabilities):
 
 def objective(frac, probs, odds, eps=1e-20):
     return -np.dot(probs, np.log2(frac[0] + frac[1:] * odds + eps))
-
-
-def compute_allocation(portfolio, odds):
-    estimates = np.asarray(portfolio.draw())
-    current_allocation = np.asarray(portfolio.allocation)
-    nonnegative = tuple([(0.0, 1.0) for _ in range(len(current_allocation))])
-    optimum = minimize(objective,
-                       current_allocation,
-                       args=(estimates, odds),
-                       bounds=nonnegative,
-                       constraints=({'type': 'eq', 'fun': lambda x: np.sum(x) - 1.})
-                       )
-    log.info('Optimal Kelly exponent: {:.4f}'.format(-optimum.fun))
-    return optimum.x
 
 
 def compute_optimal_allocation(portfolio, odds, probs):
